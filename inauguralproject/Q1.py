4bc2df3c58bee95b91596222d28f4bb0ad3335a5
Matias Hall <matias.hall2402@gmail.com>
2021-03-12 00:38:59 +0100

    OF

--- inauguralproject/Q1.py
+++ inauguralproject/Q1.py
@@ -9,23 +9,30 @@ tau_p = 0.004
 p_bar = 3
 m = 0.5
 
+#Q1
 
+
+#Utility function
 def u_func(h, c, phi = 0.3):
     return c**(1-phi)*h**phi
 
-def value_of_choice(h, phi, epsilon, r, tau_g, tau_p, p_bar, m):
-    p_thilde = h * epsilon
-    tax = r * h + tau_g * p_thilde + tau_p * max(p_thilde-p_bar, 0)
-    c = m - tax
-    return -u_func(h, c, phi)
+#Optimize function
+def u_optimize(h, phi, epsilon, r, tau_g, tau_p, p_bar, m):
+    def value_of_choice(h, phi, epsilon, r, tau_g, tau_p, p_bar, m):
+        p_thilde = h * epsilon
+        tax = r * h + tau_g * p_thilde + tau_p * max(p_thilde-p_bar, 0)
+        c = m - tax
+        return -u_func(h, c, phi)
 
 
-sol = optimize.minimize_scalar(value_of_choice, method ='brent', args = (phi, epsilon, r, tau_g, tau_p, p_bar, m))
+    sol = optimize.minimize_scalar(value_of_choice, method ='brent', args = (phi, epsilon, r, tau_g, tau_p, p_bar, m))
 
-h_star = sol.x
-p_thilde = h_star * epsilon
-tax = r * h_star + tau_g * p_thilde + tau_p * max(p_thilde-p_bar, 0)
-c_star = m - tax
-u_star = u_func(h_star, c_star, phi)
+    h_star = sol.x
+    p_thilde = h_star * epsilon
+    tax = r * h_star + tau_g * p_thilde + tau_p * max(p_thilde-p_bar, 0)
+    c_star = m - tax
+    u_star = u_func(h_star, c_star, phi)
+    return h_star, c_star, u_star
 
-print(h_star, c_star, u_star)
+
+# Q2
