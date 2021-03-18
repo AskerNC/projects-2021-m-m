93b2fcea57785de45cbfee11d8f9c9be278a666a
Matias Hall <matias.hall2402@gmail.com>
2021-03-18 16:20:17 +0100

    plot

--- inauguralproject/Q1.py
+++ inauguralproject/Q1.py
@@ -28,6 +28,7 @@ def u_func(h, c, phi = 0.3):
 
 #Optimize function
 def u_optimize(phi, epsilon, r, tau_g, tau_p, p_bar, m):
+    
     def objective(h, phi, epsilon, r, tau_g, tau_p, p_bar, m):
         p_thilde = h * epsilon
         tax = r * h + tau_g * p_thilde + tau_p * max(p_thilde-p_bar, 0)
@@ -44,10 +45,45 @@ def u_optimize(phi, epsilon, r, tau_g, tau_p, p_bar, m):
     u_star = u_func(h_star, c_star, phi)
     return h_star, c_star, u_star
 
+
 h, c, u = u_optimize(phi, epsilon, r, tau_g, tau_p, p_bar, m)
 
 print(h, c, u)
 
 # Q2
 
-hej markus
+N = 100
+m_vec = np.linspace(0.4, 2.5, N)
+h_vec = np.zeros(N)
+c_vec = np.zeros(N)
+u_vec = np.zeros(N)
+
+for i in range(N):
+    h_vec[i], c_vec[i], u_vec[i] = u_optimize(phi, epsilon, r, tau_g, tau_p, p_bar, m_vec[i])
+
+
+# create the figure
+fig = plt.figure(figsize=(10,4))# figsize is in inches...
+
+
+# left plot
+ax_left = fig.add_subplot(1,2,1)
+
+ax_left.plot(m_vec,h_vec)
+
+ax_left.set_title('h* as function of m')
+ax_left.set_xlabel('m')
+ax_left.set_ylabel('h*')
+ax_left.grid(True)
+
+# c. right plot
+ax_right = fig.add_subplot(1,2,2)
+
+ax_right.plot(m_vec,c_vec)
+
+ax_right.set_title('c* as function of m')
+ax_right.set_xlabel('m')
+ax_right.set_ylabel('c*')
+ax_right.grid(True)
+
+plt.show()
