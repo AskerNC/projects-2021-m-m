f7c55876acd3db87520b1ae1fae306d7c9d123b7
Markus Larsen <markusg@outlook.dk>
2021-03-18 17:36:37 +0100

    Q3

--- inauguralproject/Markusv2.py
+++ inauguralproject/Markusv2.py
@@ -102,14 +102,14 @@ high = 0.35
 
 def tax_burden(seed, size, low, high, phi=0.3, epsilon=0.5, r=0.03, tau_g=0.012, tau_p=0.004,p_bar=3):
     np.random.seed(seed)
-    mi = np.random.lognormal(low=low,high=high,size=size)
+    mi = np.random.lognormal(low,high,size)
 
     tax_burd = 0
 
     for i, mi in enumerate(mi):
         ch_star = u_optimize(phi, epsilon, r, tau_g, tau_p, p_bar, mi)
         tax_i = tau_g*ch_star[1] + tau_p*max(ch_star[1]-p_bar,0)
-        tax_burd += tax_i/N
+        tax_burd += tax_i/size
     return tax_burd
 
 tax_burden(seed, size, low, high)
