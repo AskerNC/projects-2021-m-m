3d0e11c2bed90705001c3246e9d2e6307bbd956c
Markus Larsen <markusg@outlook.dk>
2021-03-18 17:29:05 +0100

    Q3

--- inauguralproject/Markusv2.py
+++ inauguralproject/Markusv2.py
@@ -107,9 +107,9 @@ def tax_burden(seed, size, low, high, phi=0.3, epsilon=0.5, r=0.03 tau_g=0.012, 
     tax_burd = 0
 
     for i, mi in enumerate(mi):
-    ch_star = u_optimize(phi, epsilon, r, tau_g, tau_p, p_bar, mi)
-    tax_i = tau_g*ch_star[1] + tau_p*max(ch_star[1]-p_bar,0)
-    tax_burd += tax_i/N
+        ch_star = u_optimize(phi, epsilon, r, tau_g, tau_p, p_bar, mi)
+        tax_i = tau_g*ch_star[1] + tau_p*max(ch_star[1]-p_bar,0)
+        tax_burd += tax_i/N
     return tax_burd
 
 tax_burden(seed, size, low, high)
