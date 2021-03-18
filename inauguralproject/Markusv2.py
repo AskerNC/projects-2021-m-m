d11fd54e6a50ce2c74ab7791343d69f953328598
Markus Larsen <markusg@outlook.dk>
2021-03-18 17:31:54 +0100

    Q3

--- inauguralproject/Markusv2.py
+++ inauguralproject/Markusv2.py
@@ -100,7 +100,7 @@ size = 10000
 low = -0.4
 high = 0.35
 
-def tax_burden(seed, size, low, high, phi=0.3, epsilon=0.5, r=0.03 tau_g=0.012, tau_p=0.004,p_bar=3):
+def tax_burden(seed, size, low, high, phi=0.3, epsilon=0.5, r=0.03, tau_g=0.012, tau_p=0.004,p_bar=3):
     np.random.seed(seed)
     mi = np.random.lognormal(low=low,high=high,size=size)
 
