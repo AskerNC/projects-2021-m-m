1132e54501908a03685f4dcb0c77e421287f9282
Markus Larsen <markusg@outlook.dk>
2021-04-05 10:31:59 +0200

    !

--- dataproject/Aflevering.py
+++ dataproject/Aflevering.py
@@ -13,3 +13,9 @@ tables = Dst.get_tables(subjects=['04'])
 tables[tables.id == 'AUF01']
 unemp_pers = Dst.get_variables(table_id='AUF01')
 
+for id in ['YDELSESTYPE','ALDER','KØN']:
+    print(id)
+    values = unemp_pers.loc[unemp_pers.id == id,['values']].values[0,0]
+    for value in values:      
+        print(f' id = {value["id"]}, text = {value["text"]}')
+
