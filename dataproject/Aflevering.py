9cdcc2154fb2333c9053a580323f45def153ffd5
Markus Larsen <markusg@outlook.dk>
2021-04-05 11:55:43 +0200

    Graph

--- dataproject/Aflevering.py
+++ dataproject/Aflevering.py
@@ -42,12 +42,8 @@ unempl.loc[unempl.municipality == 'Samsø'][unempl.gender == 'Men']
 
 #Making graphs
 unempl.info()
-unempl.loc[unempl['municipality'] == 'Samsø',:].plot(x='time',y='unemployed',legend=False)
+unempl.loc[unempl['municipality'] == 'Samsø',:].plot(x='x',y='unemployed',legend=False)
 
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-
-unempl.groupby('municipality')['unemployed'].plot(ax=ax,style='-o')
 
 
 
