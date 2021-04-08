89d4e3307a68c63ec98a0a4dbd1bfb78a73df271
Matias Hall <matias.hall2402@gmail.com>
2021-04-08 17:29:37 +0200

    wack

--- dataproject/matias.py
+++ dataproject/matias.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import datetime
-
+import copy
 import pandas_datareader 
 import pydst 
 import matplotlib.pyplot as plt
@@ -13,47 +13,50 @@ Dst = pydst.Dst(lang='en')
 Dst.get_subjects()
 tables = Dst.get_tables(subjects=['04'])
 tables[tables.id == 'AUF01']
-unemp_pers = Dst.get_variables(table_id='AUF01')
+unempl_pers = Dst.get_variables(table_id='AUF01')
 
+#We find the possible IDs for the chosen variables within the table and prints them with
+#their associated text to describe the IDs
 for id in ['YDELSESTYPE','ALDER','KØN']:
-    " Finds the possible IDs for the chosen variables within the table and prints them with"
-    " their associated text to describe the IDs"
+    
     print(id)
-    values = unemp_pers.loc[unemp_pers.id == id,['values']].values[0,0]
+    values = unempl_pers.loc[unempl_pers.id == id,['values']].values[0,0]
     for value in values:      
         print(f' id = {value["id"]}, text = {value["text"]}')
 
 variables = {'OMRÅDE':['*'],'YDELSESTYPE':['TOT'],'ALDER':['TOT'],'KØN':['M','K'],'TID':['*']}
-unemp_api = Dst.get_data(table_id= 'AUF01', variables=variables)
-unemp_api.head(5)
+unempl_AUF1 = Dst.get_data(table_id= 'AUF01', variables=variables)
+unempl_AUF2 = Dst.get_data(table_id= 'AUF02', variables=variables)
 
-#Beginning of data cleaning
-unempl = unemp_api
-unempl.rename(columns = {"OMRÅDE": "municipality", "ALDER":"age", "KØN":"gender","TID":"time","INDHOLD":"unemployed"}, inplace=True)
-drop_columns = ["YDELSESTYPE", "AKASSE"] #Drops the data from YDELSESTYPE and AKASSE
-unempl.drop(drop_columns, axis=1, inplace=True)
-    #Deletes any row, where it isn't a municipality
-I = unempl.municipality.str.contains('Region')
-I |= unempl.municipality.str.contains('Province')
-I |= unempl.municipality.str.contains('All Denmark')
-unempl = unempl.loc[I == False] #Keep everything that isn't "I"
-unempl.head(10)
-unempl.loc[unempl.municipality == 'Samsø'][unempl.gender == 'Men']
+#Rewrite the TID parameter to be in datetime
+unempl_AUF1.loc[:,'TID']= pd.to_datetime(unempl_AUF1.loc[:,'TID'].str.replace('M',''),format='%Y%m')
+unempl_AUF2.loc[:,'TID']= pd.to_datetime(unempl_AUF2.loc[:,'TID'].str.replace('M',''),format='%Y%m')
+unempl_AUF2.head(-1)
+#Since AUF2 has the actual unemployment numbers, but AUF1 have more recent data, we choose to stack these on top of eachother.
+I = (unempl_AUF1["TID"] > "2019-06-01") #Choose to stack only rows in AUF1, where there are newer data than in AUF2
+unempl_AUF1 = unempl_AUF1.loc[I == True]
+unempl_AUF1
+#concat = pd.concat([unempl_AUF2, unempl_AUF1])
+outer = pd.merge(unempl_AUF1, unempl_AUF2, how='outer') 
 
-#Making graphs
-unempl.info()
-#unempl.time = unempl.time.astype(int)
+#Create seperate total unemployment df for men and women and sum them
+I = (outer['OMRÅDE'] == 'All Denmark') & (outer['KØN'] == 'Women')
+unem_tot_wom = outer.loc[I, :]
 
-unempl.time = unempl.time.astype(str)
+unem_tot_wom.head()
 
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
+I = (outer['OMRÅDE'] == 'All Denmark') & (outer['KØN'] == 'Men')
+unem_tot_men = outer.loc[I, :]
 
-unempl.groupby('time')['unemployed'].plot(ax=ax,legend=True)
+unem_tot = copy.copy(unem_tot_wom)
+unem_tot.head(100)
 
-unempl.loc[unempl['municipality'] == 'Samsø',:].plot(ax=ax,legend=True)
+unem_tot['INDHOLD'] = unem_tot_men['INDHOLD'] + unem_tot_wom['INDHOLD']
+unem_tot.head(100)
 
-fig.show()
 
+drop_columns = ["YDELSESTYPE", "AKASSE", "KØN", "ALDER"] 
+
+unem_tot = outer['All Denmark']
 
 #Graf med faktiske bruttoledige, væksten i % og den gennemsnitslige vækst i %
