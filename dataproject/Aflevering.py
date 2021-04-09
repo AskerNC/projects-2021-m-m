698d81437c80a2fc035923f5dff69dd6a87bbe2f
Markus Larsen <markusg@outlook.dk>
2021-04-09 17:01:05 +0200

    Widget for pct_change finished, amount of ticks done, missing is to shorten the str

--- dataproject/Aflevering.py
+++ dataproject/Aflevering.py
@@ -88,7 +88,8 @@ unempl_pct = unempl[unempl['gender'] == 'Total'] #create a variable only consist
 
 def plot_interact_pct(df, municipality):
     I = df['municipality'] == municipality
-    df.loc[I,:].plot(x='time', y = 'pct change', kind='bar', style='-o', legend=False, xticks=()) #xticks = empty list to avoid all dates being included
+    df.loc[I,:].plot(x='time', y = 'pct change', kind='bar', style='-o', legend=False)
+    plt.locator_params(axis='x', nbins=15) #Changes how many ticks are on the graph
 widgets.interact(plot_interact_pct, 
     df = widgets.fixed(unempl_pct),
     municipality = widgets.Dropdown(description='Municipality', 
